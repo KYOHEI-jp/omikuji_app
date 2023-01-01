6acc827abd1c1f4f00688edfcdf5838df9455b4c
KYOHEI-jp <kingdomhearts510@gmail.com>
2023-01-01 11:22:16 +0900

    転居と争い追加

--- app/src/main/java/com/example/omikuji_app/MainActivity.kt
+++ app/src/main/java/com/example/omikuji_app/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.omikuji_app.ui.theme.Omikuji_appTheme
 
@@ -21,14 +22,22 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Omikuji_appTheme {
-                // A surface container using the 'background' color from the theme
                 Column(
                     modifier = Modifier.fillMaxSize(),
-                    verticalArrangement = Arrangement.SpaceEvenly,
+                    verticalArrangement = Arrangement.Top,
                     horizontalAlignment = Alignment.CenterHorizontally,
                 ) {
                     showLucky()
-                    machibito()
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        verticalArrangement = Arrangement.SpaceEvenly,
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                    ) {
+                        machibito()
+                        battle()
+                        travel()
+                    }
+
                 }
             }
         }
@@ -51,7 +60,7 @@ fun DefaultPreview() {
 // 運勢
 @Composable
 fun showLucky() {
-    val lucky = listOf<String>("大吉","吉", "中吉", "小吉", "末吉", "凶")
+    val lucky = listOf<String>("大吉", "吉", "中吉", "小吉", "末吉", "凶")
     val luckySet = lucky.shuffled().first()
     Text(text = "運勢：${luckySet}", fontSize = 30.sp)
 }
@@ -71,9 +80,9 @@ fun showLucky() {
  */
 @Composable
 fun battle() {
-    val battle = listOf<String>("めちゃくちゃ来ます", "ドン引きするくらい来る", "誰も来ない", "後ろにいます", "あなたの隣にいる")
-    val randomPeople = people.shuffled().first()
-    Text(text = "待人：${randomPeople}", fontSize = 20.sp)
+    val battle = listOf<String>("すぐ喧嘩になる", "世界大戦の様相", "10人vs1人", "後ろにいます", "あなたの隣にいる")
+    val randomBattle = battle.shuffled().first()
+    Text(text = "争事：${randomBattle}", fontSize = 20.sp)
 }
 
 /**
@@ -91,31 +100,31 @@ fun battle() {
 /**
  * 旅行
  */
+@Composable
+fun travel() {
+    val people = listOf<String>("めちゃくちゃ来ます", "ドン引きするくらい来る", "誰も来ない", "後ろにいます", "あなたの隣にいる")
+    val randomPeople = people.shuffled().first()
+    Text(text = "旅行：${randomPeople}", fontSize = 20.sp)
+}
 
 /**
  * 待人
  */
 @Composable
 fun machibito() {
-    val people = listOf<String>("めちゃくちゃ来ます", "ドン引きするくらい来る", "誰も来ない", "後ろにいます", "あなたの隣にいる")
+    val people =
+        listOf<String>("めちゃくちゃ来る", "大勢で押し寄せる", "ドン引きするくらい来る", "誰も来ない", "後ろにいます", "あなたの隣にいる")
     val randomPeople = people.shuffled().first()
     Text(text = "待人：${randomPeople}", fontSize = 20.sp)
 }
-
 
 
 /**
  * 転居
  */
 @Composable
-fun machibito() {
+fun move() {
     val people = listOf<String>("めちゃくちゃ来ます", "ドン引きするくらい来る", "誰も来ない", "後ろにいます", "あなたの隣にいる")
     val randomPeople = people.shuffled().first()
-    Text(text = "待人：${randomPeople}", fontSize = 20.sp)
-}
-@Composable
-fun machibito() {
-    val people = listOf<String>("めちゃくちゃ来ます", "ドン引きするくらい来る", "誰も来ない", "後ろにいます", "あなたの隣にいる")
-    val randomPeople = people.shuffled().first()
-    Text(text = "待人：${randomPeople}", fontSize = 20.sp)
+    Text(text = "転居：${randomPeople}", fontSize = 20.sp)
 }
